2f1e874001ea9e4595c2adefa9a328a1a7ddb2ff
Seema Taneja <seemamehta001@gmail.com>
2022-05-30 14:18:11 +0100

    FizzBuzz in csharp

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FizzBuzz
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           Fizzbuzz fizzbuzz1 = new Fizzbuzz();
+           fizzbuzz1.DoFizzBuzz();
+        }
+    }
+}
