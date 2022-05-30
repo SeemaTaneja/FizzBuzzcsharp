bfbe50f9c64c8f703d91d8937f8fcab13edda0ec
Seema Taneja <seemamehta001@gmail.com>
2022-05-30 17:46:51 +0100

    Add rule 7,11,13

--- FizzBuzz/Fizzbuzz.cs
+++ FizzBuzz/Fizzbuzz.cs
@@ -4,27 +4,60 @@ namespace FizzBuzz
 {
    public class Fizzbuzz
    {
-      public void DoFizzBuzz()
+       private bool IsDivisibleBy(int numerator , int denominator)
+       {
+           return numerator % denominator == 0;
+       }
+       private bool StartWithB(string testString)
+       {
+           return testString[0] == 'B';
+       }
+      public void DoFizzBuzz(int maxNumber)
       {
-         for(int i=1; i<=100; i++)
-         {
-            if(i%3==0 && i%5==0)
-             {
-                Console.WriteLine("FizzBuzz");
-             }
-           else if(i%3==0)
-           {
-                Console.WriteLine("Fizz");
-           }
-           else if(i%5==0)
-           {
-                Console.WriteLine("Buzz");
-           }
-           else
-           {
-                Console.WriteLine(i);
-           }
-         }
+          for(int i=1; i<=maxNumber ; i++)
+          {
+              Console.WriteLine(ForNumber(i));
+          }
+      }
+      public string ForNumber(int number)
+      {
+            List<string> result =new List<string>();
+            if(IsDivisibleBy(number,3))
+            {
+                result.Add("Fizz");
+            }
+             if(IsDivisibleBy(number,5))
+            {
+                result.Add("Buzz");
+            }
+            if(IsDivisibleBy(number,7))
+            {
+                result.Add("Bang");
+            }
+            if(IsDivisibleBy(number,11))
+            {
+                result = new List<string>(){"Bong"};
+            }
+            if(IsDivisibleBy(number,13))
+            {
+               int firstBIndex = result.FindIndex(StartWithB);
+               if(firstBIndex == -1)
+               {
+                   result.Add("Fezz");
+               }
+               else
+               {
+                   result.Insert(firstBIndex,"Fezz");
+               }  
+            }
+            if(result.Count==0)
+            {
+                return number.ToString();
+            }
+            else
+            {
+                return string.Join("",result);
+            }
+      }
       }
    }
-}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -5,8 +5,18 @@ namespace FizzBuzz
     {
         public static void Main(string[] args)
         {
+            int maxNumber;
+            if(args.Length==0)
+            {
+                maxNumber = 100;
+            }
+            else
+            {
+                maxNumber = int.Parse(args[0]);
+            }
+
            Fizzbuzz fizzbuzz1 = new Fizzbuzz();
-           fizzbuzz1.DoFizzBuzz();
+           fizzbuzz1.DoFizzBuzz(maxNumber);
         }
     }
 }
